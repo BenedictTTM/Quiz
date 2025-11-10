7b893473e24b6f891f58f7ea39cc1dd4556beb4f
Benedict <palomakut1@gmail.com>
2025-11-10 19:32:02 +0000

    s

---
+++ app/Components/Footer.js
@@ -0,0 +1,18 @@
+'use client';
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="poppins bg-gray-100 mt-auto">
+      <div className="mx-auto max-w-screen-xl p-4 sm:px-8 sm:py-5 lg:px-10">
+        <div className="text-center">
+          <p className="text-sm text-gray-600">
+            powered by <span className="font-semibold text-gray-800">enginx</span>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
